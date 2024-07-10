c478ace5cea38140ee40262f594261d441797053
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2024-07-10 17:33:55 +0100

    .

--- hngUser/hngUser/settings.py
+++ hngUser/hngUser/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4ntfuhe%h(=ej1(w6uke=c4b#)((&^4vl#ksuz3$37t+(kc$r%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
