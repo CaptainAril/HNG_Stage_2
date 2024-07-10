a653640cb72c48aaf8c0112fa42a987a41eb2f21
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2024-07-10 15:32:46 +0100

    HNG-11 Stage 2

--- install.sh
+++ install.sh
@@ -4,4 +4,9 @@ python3 -m venv venv
 source venv/bin/activate
 python3.12 -m pip install --upgrade pip
 pip3 install -r requirements.txt
-python3 hngUser/manage.py runserver
+cd hngUser
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py runserver
+# end of file
+
