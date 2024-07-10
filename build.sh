5020b7a56510736d1356d761e2289baf17e313ba
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2024-07-10 18:54:07 +0100

    .

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ echo "Building the project..."
 python3.12 -m pip install -r requirements.txt
 echo "Make Migration..."
 python3.12 hngUser/manage.py makemigrations --noinput
-python3.12 hngUsermanage.py migrate --noinput
+python3.12 hngUser/manage.py migrate --noinput
 echo "Collect Static..."
 python3.12 hngUser/manage.py collectstatic --noinput --clear
