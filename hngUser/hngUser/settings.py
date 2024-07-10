1a98f86435a76f9c73333f6dd240e2e0e725d333
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2024-07-10 17:31:27 +0100

    .

--- hngUser/hngUser/settings.py
+++ hngUser/hngUser/settings.py
@@ -78,12 +78,24 @@ WSGI_APPLICATION = 'hngUser.wsgi.app'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hng_db',
+        'USER': 'hng_dev',
+        'PASSWORD': 'hng_dev_123',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
+
 
 
 # Password validation
